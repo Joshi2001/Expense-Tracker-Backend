daf5967f87f28ce224c060438799ba03f1bf75f3
Shubham <shubhamjoshi12050@gmail.com>
2026-04-27 00:39:19 +0530

    Add feature

--- server.js
+++ server.js
@@ -2,26 +2,10 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
-// Debug (deploy ke baad hata dena)
-console.log("ENV CHECK:", {
-  mongo: process.env.MONGO_URI ? "OK" : "MISSING",
-  jwt: process.env.JWT_SECRET ? "OK" : "MISSING",
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-const startServer = async () => {
-  try {
-    await connectDB();
-    console.log("MongoDB Connected");
-
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error("Startup error:", err.message);
-    process.exit(1);
-  }
-};
-
-startServer();
