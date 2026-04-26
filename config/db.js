2e7897ab566b0408c8a2a6227a0125b3dbbad575
Shubham <shubhamjoshi12050@gmail.com>
2026-04-27 00:37:33 +0530

    Add feature

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  return mongoose.connect(process.env.MONGO_URI);
+  return mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000, // fail fast with clear error
+  });
 };
-
 module.exports = connectDB;
