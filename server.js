ab83e619fca352d34444f30ff8030b743ef8a93b
Shubham <shubhamjoshi12050@gmail.com>
2026-04-27 10:53:44 +0530

    update

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 require("dotenv").config();
-const app = require("./app");
+const express = require("express");
+const app = express();
 const connectDB = require("./config/db");
 
+// Import routes
+const authRoutes = require("./routes/authRoutes"); // path check kar lena
+
+// Connect Database
 connectDB();
 
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes); // 👈 yaha tera signup/login connect hoga
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Port
 const PORT = process.env.PORT || 5000;
 
+// Server start
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
